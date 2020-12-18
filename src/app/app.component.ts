a5724d956f2004c7d3529c30361c8b7286e8a2b1
Dilip Kakadiya <dil.kakadiya@gmail.com>
2020-12-18 19:32:02 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   }
 
   constructor(private _http: HttpClient) {
-    this._http.get<Product[]>('https://pedantic-galileo-1263ff.netlify.app/api/products')
+    this._http.get<Product[]>('/api/products')
       .subscribe(
         (response) => {
           console.log(response);
