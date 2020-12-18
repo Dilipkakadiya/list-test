458450dd69ab23a0c0fb676f486be1d8e12d8406
Dilip Kakadiya <dil.kakadiya@gmail.com>
2020-12-18 20:06:20 +0530

    Update custom-webpack.config.js

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,6 +1,7 @@
 const json = require('json-server');
 
 module.exports = {
+  mode: 'production',
   devServer: {
     before: function(app) {
       app.use('/api', json.router('https://pedantic-galileo-1263ff.netlify.app/assets/db.json'));
