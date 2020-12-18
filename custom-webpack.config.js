f3148d276787dea3f37b62421b905d52850fb452
Dilip Kakadiya <dil.kakadiya@gmail.com>
2020-12-18 19:39:47 +0530

    Update custom-webpack.config.js

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,9 +1,8 @@
 const json = require('json-server');
 
 module.exports = {
-  mode: 'production',
   devServer: {
-    before: function (app) {
+    before: function(app) {
       app.use('/api', json.router('src/assets/db.json'));
     }
   }
